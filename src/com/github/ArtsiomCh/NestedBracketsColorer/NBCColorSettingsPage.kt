b5cc8099a26f93292865af69f646f21eec6e190a
Artsiom Chapialiou <achapialiou@gmail.com>
2018-04-29 23:46:10 -0400

    ColorSettingPage added for colors customisation

---
+++ src/com/github/ArtsiomCh/NestedBracketsColorer/NBCColorSettingsPage.kt
@@ -0,0 +1,77 @@
+package com.github.ArtsiomCh.NestedBracketsColorer
+
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.PlainTextSyntaxHighlighterFactory
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.options.colors.AttributesDescriptor
+import com.intellij.openapi.options.colors.ColorDescriptor
+import com.intellij.openapi.options.colors.ColorSettingsPage
+import com.intellij.util.containers.ContainerUtil
+import java.util.*
+import javax.swing.Icon
+
+internal val roundBracketLevel2 = TextAttributesKey.createTextAttributesKey(
+        "round bracket level 2", DefaultLanguageHighlighterColors.STRING)
+internal val roundBracketLevel3 = TextAttributesKey.createTextAttributesKey(
+        "round bracket level 3", DefaultLanguageHighlighterColors.NUMBER)
+internal val roundBracketLevel4 = TextAttributesKey.createTextAttributesKey(
+        "round bracket level 4", DefaultLanguageHighlighterColors.DOC_COMMENT_TAG_VALUE)
+
+internal val squareBracketLevel2 = TextAttributesKey.createTextAttributesKey(
+        "square bracket level 2", DefaultLanguageHighlighterColors.INSTANCE_FIELD)
+internal val squareBracketLevel3 = TextAttributesKey.createTextAttributesKey(
+        "square bracket level 3", DefaultLanguageHighlighterColors.INSTANCE_METHOD)
+
+class NBCColorSettingsPage : ColorSettingsPage {
+
+    private val DESCRIPTORS = arrayOf(
+        AttributesDescriptor("Round bracket level 2", roundBracketLevel2),
+        AttributesDescriptor("Round bracket level 3", roundBracketLevel3),
+        AttributesDescriptor("Round bracket level 4", roundBracketLevel4),
+            AttributesDescriptor("Square bracket level 2", squareBracketLevel2),
+            AttributesDescriptor("Square bracket level 3", squareBracketLevel3)
+    )
+
+    override fun getAttributeDescriptors(): Array<AttributesDescriptor> {
+        return DESCRIPTORS
+    }
+
+    override fun getAdditionalHighlightingTagToDescriptorMap(): MutableMap<String, TextAttributesKey> {
+        return ContainerUtil.newHashMap(
+                Arrays.asList("r2", "r3", "r4", "s2", "s3"),
+                Arrays.asList(
+                        roundBracketLevel2,
+                        roundBracketLevel3,
+                        roundBracketLevel4,
+                        squareBracketLevel2,
+                        squareBracketLevel3
+                )
+        )
+    }
+
+    override fun getDisplayName(): String {
+        return "Nested Brackets Colorer"
+    }
+
+    override fun getDemoText(): String {
+        return  "Round () brackets:\n" +
+                "level_1: ( level_2: <r2>(</r2> level_3: <r3>(</r3> level_4: <r4>(</r4>" +
+                "<r4>)</r4><r3>)</r3><r2>)</r2>)\n\n" +
+                "Square [] brackets:\n" +
+                "level_1: [ level_2: <s2>[</s2> level_3: <s3>[</s3> <s3>]</s3><s2>]</s2>]\n"
+    }
+
+    override fun getIcon(): Icon? {
+        return null
+    }
+
+    override fun getColorDescriptors(): Array<ColorDescriptor> {
+        return ColorDescriptor.EMPTY_ARRAY
+    }
+
+    override fun getHighlighter(): SyntaxHighlighter {
+        return PlainTextSyntaxHighlighterFactory().getSyntaxHighlighter( null, null)
+    }
+
+}
--- src/com/github/ArtsiomCh/NestedBracketsColorer/NestedBracketsColorerUtils.kt
+++ src/com/github/ArtsiomCh/NestedBracketsColorer/NestedBracketsColorerUtils.kt
@@ -1,28 +1,26 @@
 package com.github.ArtsiomCh.NestedBracketsColorer
 
 import com.intellij.lang.annotation.AnnotationHolder
-import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.tree.IElementType
-import java.awt.Color
-import java.awt.Font
 
 internal val roundBrackets = arrayOf(
-        Color(255, 255, 255),
-        Color(0x6A8759),
-        Color(0x6897BB)
+        roundBracketLevel4,
+        roundBracketLevel2,
+        roundBracketLevel3
 )
 
 internal val squareBrackets = arrayOf(
-        Color(255, 255, 255),
-        Color(150, 80, 150)
-//        Color(255, 152, 99)
+        squareBracketLevel3,
+        squareBracketLevel2
 )
 
 internal fun annotateUtil(element: PsiElement, holder: AnnotationHolder,
-                          LEFT: IElementType, RIGHT: IElementType, color: Array<Color>) {
+                          LEFT: IElementType, RIGHT: IElementType,
+                          brackets: Array<TextAttributesKey>) {
 
     fun getBracketLevel(element: LeafPsiElement): Int {
         var level = if (element.elementType == RIGHT) 1 else 0
@@ -51,7 +49,7 @@ internal fun annotateUtil(element: PsiElement, holder: AnnotationHolder,
         return level
     }
 
-    fun getColor (level: Int) = color[(level-1) % color.size]
+    fun getBracketTextAttributesKey (level: Int) = brackets[(level-1) % brackets.size]
 
     if (element is LeafPsiElement) {
         val level = when (element.elementType) {
@@ -59,8 +57,8 @@ internal fun annotateUtil(element: PsiElement, holder: AnnotationHolder,
             else -> 0
         }
         if (level > 1) {
-            holder.createInfoAnnotation(element as PsiElement, null).enforcedTextAttributes =
-                    TextAttributes( getColor(level), null, null, null, Font.PLAIN)
+            holder.createInfoAnnotation(element as PsiElement, null).textAttributes =
+                    getBracketTextAttributesKey( level)
         }
     }
 }
