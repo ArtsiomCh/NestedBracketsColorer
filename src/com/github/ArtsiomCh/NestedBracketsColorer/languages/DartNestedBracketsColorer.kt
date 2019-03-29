ef720c33b3fb364dc461966f8295e596ed9d8c81
Artsiom Chapialiou <achapialiou@gmail.com>
2019-03-29 17:46:25 -0400

    Dart support added

---
+++ src/com/github/ArtsiomCh/NestedBracketsColorer/languages/DartNestedBracketsColorer.kt
@@ -0,0 +1,15 @@
+package com.github.ArtsiomCh.NestedBracketsColorer.languages
+
+import com.github.ArtsiomCh.NestedBracketsColorer.*
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.psi.PsiElement
+import com.jetbrains.lang.dart.DartTokenTypes.*
+
+class DartNestedBracketsColorer : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        annotateUtil(element, holder, LPAREN, RPAREN, roundBrackets)
+        annotateUtil(element, holder, LT, GT, roundBrackets)
+    }
+
+}
