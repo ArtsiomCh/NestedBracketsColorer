ba9228bb7cf781ada1b066198fbb79fba4e90a0c
Artsiom Chapialiou <achapialiou@gmail.com>
2021-12-05 17:42:55 +0300

    0.6.0 update highlighting mechanism to follow the latest JB recommendations

--- src/com/github/ArtsiomCh/NestedBracketsColorer/NestedBracketsColorerUtils.kt
+++ src/com/github/ArtsiomCh/NestedBracketsColorer/NestedBracketsColorerUtils.kt
@@ -1,6 +1,7 @@
 package com.github.ArtsiomCh.NestedBracketsColorer
 
 import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -8,26 +9,28 @@ import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.psi.tree.IElementType
 
 internal val roundBrackets = arrayOf(
-        roundBracketLevel4,
-        roundBracketLevel2,
-        roundBracketLevel3
+    roundBracketLevel4,
+    roundBracketLevel2,
+    roundBracketLevel3
 )
 
 internal val squareBrackets = arrayOf(
-        squareBracketLevel3,
-        squareBracketLevel2
+    squareBracketLevel3,
+    squareBracketLevel2
 )
 
-internal fun annotateUtil(element: PsiElement, holder: AnnotationHolder,
-                          LEFT: IElementType, RIGHT: IElementType,
-                          brackets: Array<TextAttributesKey>) {
+internal fun annotateUtil(
+    element: PsiElement, holder: AnnotationHolder,
+    LEFT: IElementType, RIGHT: IElementType,
+    brackets: Array<TextAttributesKey>
+) {
 
     fun getBracketLevel(element: LeafPsiElement): Int {
         var level = if (element.elementType == RIGHT) 1 else 0
 
         tailrec fun iterateParents(currentNode: PsiElement) {
 
-            tailrec fun iterateChilds(currentChild: PsiElement){
+            tailrec fun iterateChilds(currentChild: PsiElement) {
                 if (currentChild is LeafPsiElement) {
                     when (currentChild.elementType) {
                         LEFT -> level++
@@ -49,7 +52,7 @@ internal fun annotateUtil(element: PsiElement, holder: AnnotationHolder,
         return level
     }
 
-    fun getBracketTextAttributesKey (level: Int) = brackets[(level-1) % brackets.size]
+    fun getBracketTextAttributesKey(level: Int) = brackets[(level - 1) % brackets.size]
 
     if (element is LeafPsiElement) {
         val level = when (element.elementType) {
@@ -57,8 +60,10 @@ internal fun annotateUtil(element: PsiElement, holder: AnnotationHolder,
             else -> 0
         }
         if (level > 1) {
-            holder.createInfoAnnotation(element as PsiElement, null).textAttributes =
-                    getBracketTextAttributesKey( level)
+            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                .range(element as PsiElement)
+                .textAttributes(getBracketTextAttributesKey(level))
+                .create()
         }
     }
 }
